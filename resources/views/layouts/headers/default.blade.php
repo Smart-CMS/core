8faa4e131b24837f1fd4ef5687d803ff7e91cdb9
maxboyko <maxboiko@divotek.com>
2025-05-01 14:59:52 +0300

    fix header, and action name

--- resources/views/layouts/headers/default.blade.php
+++ resources/views/layouts/headers/default.blade.php
@@ -6,10 +6,3 @@
         ]
     }
 @endsection_meta --}}
-<div class="container-fluid" @click="alert('test')">
-    <div class="row">
-        <div class="col-12">
-            <h1>Default header</h1>
-        </div>
-    </div>
-</div>
